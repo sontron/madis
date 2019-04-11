1969161557493e00a5af76322eaa7dddc8f51e2d
sontron <sontron@foxmail.com>
2019-04-11 12:30:53 +0800

    oops

--- R/glmS.R
+++ R/glmS.R
@@ -6,14 +6,16 @@
 #' @export
 
 glmS<-function(Formula,
-                data,
-                weightsVar=1,
-                subset='all',
-                Family=c('gaussian','binomial','poisson'),
-                na.action=na.rm,
-                lower='~1'
+               data,
+               weightsVar=1,
+               subset='all',
+               Family=c('gaussian','binomial','poisson'),
+               na.action=na.rm,
+               lower='~1',
+               nomoat='0.1;0.2;0.3;0.5;0.7;0.8;0.9'
 ){
   require('stringi')
+  require('rms')
   stri_replace_all_regex(Formula,'[~+]{2,}','~')->Formula
   require(fBasics)
   if(is.character(data)) data=eval(as.name(data))
@@ -38,17 +40,37 @@ glmS<-function(Formula,
     na.omit(dat)->dat
     Wt=dat[,weightsVar]
   }
-  
   if(Family=='binomial'){
     as.numeric(as.factor(dat[,yVar]))-1->dat[,yVar]
   }
-  
   glm(Formula,data=dat,family=Family,x=T,y=T,weights=Wt)->fit
-  
   step(fit,scope=list(upper=Form1,lower=Form2),trace=F)->fitStep
   
+  if(Family=='binomial'){
+    as.numeric(unlist(stri_split_fixed(nomoat,';')))->nomoAt
+    
+    # datadist(dat)->dd
+    # options(datadist='dd')
+    # lrm(Formula,data=dat)->fitLrmFull
+    # nomogram(fitLrmFull,fun=function(x)1/(1+exp(-x)),fun.at = nomoAt)->nomoFull
+    # 
+    # lrm(formula(fitStep),data=dat)->fitLrmStep
+    # 
+    # nomogram(fitLrmStep,fun=function(x)1/(1+exp(-x)),fun.at = nomoAt)->nomoStep
+    
+  }
   
-  return(list(glmResFull=fit,glmResStep=fitStep))
+  
+  
+  
+  if(Family=='binomial'){
+    # return(list(glmResFull=fit,glmResStep=fitStep,nomogramFull=nomoFull,nomogramStep=nomoStep))
+    return(list(glmResFull=fit,glmResStep=fitStep))
+  } else {
+    return(list(glmResFull=fit,glmResStep=fitStep))
+    
+  }
+  
   
   
 }
